34e8dba3b83790da699dbc74b14e2c70625f36ae
Rohit Priyadarshi <56641101+Royalaviation18@users.noreply.github.com>
2024-04-29 18:13:33 +0530

    spacing

--- src/main/java/com/royalaviation/OrderM/controller/OrderController.java
+++ src/main/java/com/royalaviation/OrderM/controller/OrderController.java
@@ -20,26 +20,25 @@ import java.util.Date;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
-
 @RestController
 public class OrderController {
+    
     @Autowired
     OrderService orderService;
 
-    //get All the orders
+    // get All the orders
     @GetMapping("orders")
-    public List<Order> getAllCompany(){
+    public List<Order> getAllCompany() {
         return orderService.readOrders();
     }
 
-    //creating an order
+    // creating an order
     @PostMapping("orders")
-    public String postMethodName(@RequestBody Order order){
+    public String postMethodName(@RequestBody Order order) {
         return orderService.createOrder(order);
     }
 
-    //deleting an orders
+    // deleting an orders
     @DeleteMapping("orders/{id}")
     public String deleteOrder(@PathVariable Long id) {
         if (orderService.deleteOrder(id))
@@ -47,40 +46,41 @@ public class OrderController {
         return "Not found";
     }
 
-    //updating an order
+    // updating an order
     @PutMapping("orders/{id}")
     public String putMethodName(@PathVariable Long id, @RequestBody Order order) {
         return orderService.updateOrder(id, order);
     }
 
-    //searching for an order based on order id
+    // searching for an order based on order id
     @GetMapping("orders/{id}")
     public Order getAllOrderById(@PathVariable Long id) {
         return orderService.readOrder(id);
     }
 
-    //searching for an order based on company id
+    // searching for an order based on company id
     @GetMapping("orders/cbased/{companyId}")
     public List<Order> getAllOrderByCompanyId(@PathVariable Long companyId) {
         return orderService.findByCompanyEntityId(companyId);
     }
 
-    //searching for an order based on customer'sname
+    // searching for an order based on customer'sname
     @GetMapping("orders/customerName")
     public List<Order> getAllOrderByName(@RequestParam("customerName") String customerName) {
         return orderService.findByCustomerEntityCustomerName(customerName);
-    }   
+    }
 
     // @GetMapping("/orders")
     // public List<Order> getOrdersByCompanyIdAndCreatedAtGreaterThanEqual(
-    //         @RequestParam Long companyId,
-    //         @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
-    //     return orderService.findByCompanyEntityIdAndCreatedAtGreaterThanEqual(companyId, date);
+    // @RequestParam Long companyId,
+    // @RequestParam @DateTimeFormat(pattern = "yyyy-MM-dd") Date date) {
+    // return
+    // orderService.findByCompanyEntityIdAndCreatedAtGreaterThanEqual(companyId,
+    // date);
     // }
-    
 
-
-    //getting reports based on companyId
+    // getting reports based on companyId
+    // gettting reports of a company based on there ids
     @GetMapping("orders/export/{companyId}")
     public void exportIntoExcelFileByCompany(@PathVariable Long companyId, HttpServletResponse response)
             throws IOException {
@@ -95,10 +95,9 @@ public class OrderController {
         List<Order> listOfOrders = orderService.findByCompanyEntityId(companyId);
         OrderReport orderReport = new OrderReport(listOfOrders);
         orderReport.generateExcelFile(response);
-
     }
 
-    //getting reports for all the company
+    // getting reports for all the company
     @GetMapping("orders/export")
     public void exportIntoExcelFile(HttpServletResponse response) throws IOException {
         response.setContentType("application/octet-stream");
